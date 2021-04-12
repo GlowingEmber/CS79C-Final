fc55f41bbc56af2281eed951a4594cacd0502484
Ethan Hopkins <58455740+GlowingEmber@users.noreply.github.com>
2021-04-11 18:36:52 -0700

    getrandom

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 // CS 79C
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+
+app.post("/", (req, res) => {
+  /*
+This is an example snippet - you should consider tailoring it
+to your service.
+*/
+  const name = req.body.name;
+
+  async function fetchGraphQL(operationsDoc, operationName, variables) {
+    const result = await fetch("http://localhost:8080/v1/graphql", {
+      method: "POST",
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName,
+      }),
+    });
+
+    return await result.json();
+  }
+
+  const operationsDoc = `query MyQuery {
+    Facts(where: {id: {_eq: ${getRandomInt(2)}}}) {
+      fact
+    }
+  }`;
+
+  function fetchMyQuery() {
+    return fetchGraphQL(operationsDoc, "MyQuery", {});
+  }
+
+  async function startFetchMyQuery() {
+    const { errors, data } = await fetchMyQuery();
+
+    if (errors) {
+      // handle those errors like a pro
+      console.error(errors);
+    }
+
+    // do something great with this precious data
+    console.log(data);
+    res.send(data);
+  }
+
+  startFetchMyQuery();
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App running on port ${port}.`)
 })
+
